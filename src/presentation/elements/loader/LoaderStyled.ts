5cdd9b930cb6fe2c6dbf3a82ab2a3e53cc502132
Elyamira <itiselya@gmail.com>
2022-09-02 10:10:28 +0100

    style: remove unnecessary styles

--- src/presentation/elements/loader/LoaderStyled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const LoaderStyled = styled.div`
-position: absolute;
-top: 50%;
-left: 50%;
-height: 100vh;
-`
--- styles/WrapperForContentWithoutFooter.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const WrapperForContentWithoutFooter = styled.div`
-flex: 1 0 auto;
-`
