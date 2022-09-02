45444952d70381d78c00d6e9b07ff94d1fd5371f
Elyamira <itiselya@gmail.com>
2022-09-02 10:09:02 +0100

    style: styles to center content & sticky footer

---
+++ styles/utilities/CenteredContentWrapper.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CenteredContentWrapper = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 100vh;
+width: 100%;
+font-size: 3rem;
+text-align: center;
+flex-direction: column;
+`
